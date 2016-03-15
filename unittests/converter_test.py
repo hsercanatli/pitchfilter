d8716a8db6b0c1236c5d07d9a4d800e14c227482
Sertan Senturk <contact@sertansenturk.com>
2016-03-15 12:09:42 +0100

    Fixed the PEP8 violations in the unittests

--- unittests/converter_test.py
+++ unittests/converter_test.py
@@ -3,10 +3,11 @@ import numpy
 import json
 from pitchfilter.PitchFilter import PitchFilter
 
+
 def test_pitch_filter():
     # reading extracted pitch from json
-    pitch = numpy.array(json.load(open(os.path.join("sample_data",
-                     "e72db0ad-2ed9-467b-88ae-1f91edcd2c59.json"), 'r')))
+    pitch = numpy.array(json.load(open(os.path.join(
+        "sample_data", "e72db0ad-2ed9-467b-88ae-1f91edcd2c59.json"), 'r')))
 
     # filtering the extracted pitch
     flt = PitchFilter()
