b7a3b432dd14ca1ede0546e1837ace4e1a86f046
Sertan Senturk <contact@sertansenturk.com>
2016-03-16 01:32:12 +0100

    Class definition clarification

--- pitchfilter/PitchFilter.py
+++ pitchfilter/PitchFilter.py
@@ -3,7 +3,7 @@ import numpy as np
 __author__ = 'hsercanatli'
 
 
-class PitchFilter:
+class PitchFilter(object):
     def __init__(self, lower_interval_thres=0.7, upper_interval_thres=1.3,
                  min_chunk_size=40, min_freq=64, max_freq=1024):
 
