9a4afaabdde29f6926e1c34c7b90f93771304e39
Sertan Senturk <contact@sertansenturk.com>
2016-05-08 05:40:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pitchfilter',
-    version='1.2.0',
+    version='1.2.1',
     description='Post filtering on pitch tracks',
     author='Hasan Sercan Atli',
     url='https://github.com/hsercanatli/pitch-post-filter',
